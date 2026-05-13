1a11d7835e9783ad2e4ab53b7a2a63a7250e1aa2
louisdelange2-max <louisdelange2@gmail.com>
2026-05-13 20:24:49 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='hfq-delivery-fix-v8';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html']).catch(()=>null)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
