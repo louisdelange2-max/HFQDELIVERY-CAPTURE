cfba44a2bd6e8cb23b076a2dc0dadd2816f07e7c
louisdelange2-max <louisdelange2@gmail.com>
2026-05-13 20:16:41 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='hfq-delivery-modern-blue-ui-v7';
+const CACHE='hfq-delivery-modern-blue-ui-v8';
 const ASSETS=['./','./index.html','./manifest.json','./icons/hfq-logo.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.indexOf('hfq-delivery-capture')===0).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
